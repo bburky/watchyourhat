9e2189af71163543fbdc92954bdbfef495b5a93a
Sterling West <sterling-west@utulsa.edu>
2012-11-04 10:15:36 -0600

    asdf

--- main3.py
+++ main3.py
@@ -15,8 +15,147 @@ from Helicopter import Helicopter
 from Text import Text
 from HealthBar import HealthBar
 import time
-import threading
-import multiplayer
+#import threading
+#import multiplayer
+
+# Sterling's tired. This is his network code. Sorry.
+
+
+hero = None
+
+#all_generated = False
+
+#enemies_list = {}
+#enemies_list_lock = threading.Lock()
+#block_lock = threading.Lock()
+#enemies_n = 1
+
+def manage_network():
+    global hero
+    my_id = -1
+    if multiplayer.hosting: my_id = 0
+    allies2 = {}
+    while not all_generated: time.sleep(.5)
+    
+    enemy_update_time = time.time()
+    while True:
+        
+        # Send position updates
+        x, y = hero.truePos
+        theta = hero.theta
+        if my_id > -1:
+            multiplayer.s.send('1 %d %d %d %d;' % (my_id, x, y, theta))
+
+        if multiplayer.hosting:
+            for i in multiplayer.pos:
+                x, y, theta = multiplayer.pos[i]
+                multiplayer.s.send('1 %d %d %d %d;' % (i, x, y, theta))
+            
+            #print len(enemies_list)
+            if time.time() - enemy_update_time > 1:
+                enemy_update_time = time.time()
+                #enemies_list_lock.acquire()
+                for i in enemies_list:
+                    en = enemies_list[i]
+                    x, y = en.truePos
+                    #tgt = en.target.n
+                    multiplayer.s.send('2 1 %d %d %d 0;' % (i, x, y))
+                #enemies_list_lock.release()
+
+        # Harvest Messages
+        multiplayer.msg_lock.acquire()
+
+        for m in multiplayer.msg_buff:
+            #print "harvesting message: %s" % m
+            m = m.split(' ')
+            m_t = int(m[0])
+
+            if m_t == 0:
+                my_id = int(m[1])
+            elif m_t == 1:
+                if len(multiplayer.msg_buff) > 500: continue
+                i = int(m[1])
+                if i == my_id or my_id == -1: continue
+                
+                if i not in multiplayer.pos:
+                    allies2[i] = addAlly()
+                    allies2[i].n = i
+                    print "New player rendered"
+                    if multiplayer.hosting:
+                        for b in upper:
+                            new_m = '3 %d %d %d;' % (b[0], b[1], seeds[b])
+                            multiplayer.s.send(new_m)
+                multiplayer.pos[i] = tuple(int(j) for j in m[2:])
+                allies2[i].truePos = list(multiplayer.pos[i][:2])
+                allies2[i].theta = multiplayer.pos[i][2]
+            elif m_t == 2:
+                if len(multiplayer.msg_buff) > 500: continue
+                en_t = int(m[1])
+                
+                i, x, y, tgt = [int(i) for i in m[2:]]
+                
+                #enemies_list_lock.acquire()
+                if i not in enemies_list:
+                    dx, dy = mapgen.tiles[1][0]
+                    wid = hei = Config['PIXELS_PER_TILE']
+                    rec = Rect((dx*wid, dy*hei), (wid, hei))
+                    img = ssTop.image_at(rec)
+        
+                    #truePos = [block[0]*Config['PIXELS_PER_BLOCK']+e[0]*45, block[1]*Config['PIXELS_PER_BLOCK']+e[1]*45]
+                    truePos = [x, y]
+                    spr = ethunterone(*truePos)
+                    spr.setCamera(hero)
+                    spr.setOffset((SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+                    spr.update(0)
+                    enemies.add(spr)
+                    actors.add(spr)
+                    active.add(spr)
+                    spr.target = hero
+                    spr.n = i
+                    
+                    middle[(0,0)].add(spr)
+                    #gpEnem.add(spr)
+                    
+                    hb = HealthBar(target=spr)
+                    actors.add(hb)
+                    active.add(hb)
+                    
+                    spr.clientUpdate = True
+                    enemies_list[i] = spr
+                
+                spr.target = allies2[tgt]
+                enemies_list[i].truePos = [x, y]
+                #enemies_list_lock.release()
+            elif m_t == 3:
+                x, y, sd = [int(i) for i in m[1:]]
+                if (x, y) in lower: continue
+                seeds[(x, y)] = sd
+                loadBlock((x, y))
+            elif m_t == 4:
+                x, y = [int(i) for i in m[1:]]
+                if (x, y) not in lower: continue
+                unloadBlock((x, y))
+            elif m_t == 5:
+                i, dmg = [int(i) for i in m[1:]]
+                enemies_list[i].damage(dmg)
+            elif m_t == 6:
+                i, dmg = [int(i) for i in m[1:]]
+                allies2[i].damage(dmg)
+        del multiplayer.msg_buff[:]
+        multiplayer.msg_lock.release()
+        
+        time.sleep(.05)
+
+
+#multi_args = 1
+#if '--fullscreen' in sys.argv: multi_args += 1
+
+#if len(sys.argv) <= multi_args:
+#    multiplayer.init_network()
+#else:
+#    multiplayer.init_network(sys.argv[-1])
+#threading.Thread(target=manage_network).start()
+# End of this selection of Sterling's bad choices.
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -112,6 +251,8 @@ bulletsText.createImage()
 
 clock = pygame.time.Clock()
 
+all_generated = True
+
 def handleEvents(events):
     for e in events:
         if e.type == KEYDOWN:
@@ -159,6 +300,7 @@ def visibleBlocks(pos):
            ])
 
 def refreshScreen():
+    #block_lock.acquire()
     screen.fill(Config['BG_COLOR'])
     changes = []
     vis = visibleBlocks(hero.truePos)
@@ -173,10 +315,12 @@ def refreshScreen():
     for u in upper:
         if u in vis:
             upper[u].draw(screen)
+    #block_lock.release()
     gui.draw(screen)
     pygame.display.flip()
 
 def generateTiles(block):
+    global enemies_n
     gpBack = pygame.sprite.RenderUpdates()
     gpFore = pygame.sprite.RenderUpdates()
     gpEnem = pygame.sprite.RenderUpdates()
@@ -184,7 +328,7 @@ def generateTiles(block):
     prevGenerated = True
     if block not in seeds:
         prevGenerated = False
-        seeds[block] = random.randrange(1, 10000000)
+        seeds[block] = random.randrange(0,10000000)
 
     bg, fg, en = mapgen.gen_block(seeds[block])
     for b in bg:
@@ -213,13 +357,14 @@ def generateTiles(block):
         active.add(spr)
         gpFore.add(spr)
 
+    #if not prevGenerated and multiplayer.hosting:
     if not prevGenerated:
         for e in en:
             x, y = mapgen.tiles[en[e]][0]
             wid = hei = Config['PIXELS_PER_TILE']
             rec = Rect((x*wid, y*hei), (wid, hei))
             img = ssTop.image_at(rec)
-
+            
             truePos = [block[0]*Config['PIXELS_PER_BLOCK']+e[0]*45, block[1]*Config['PIXELS_PER_BLOCK']+e[1]*45]
             spr = cat(*truePos)
             spr.setCamera(hero)
@@ -235,19 +380,29 @@ def generateTiles(block):
             actors.add(hb)
             active.add(hb)
 
+            #enemies_list_lock.acquire()
+            #enemies_list[enemies_n] = spr
+            #spr.n = enemies_n
+            #enemies_n += 1
+            #enemies_list_lock.release()
+
     return gpBack, gpFore, gpEnem
 
 def loadBlock(b):
+    #block_lock.acquire()
     l, u, m = generateTiles(b)
     lower[b], upper[b] = l, u
     if b not in middle: middle[b] = m
+    #block_lock.release()
 
 def unloadBlock(b):
+    #block_lock.acquire()
     for s in lower[b]:
         s.kill()
     for s in upper[b]:
         s.kill()
     del lower[b], upper[b]
+    #block_lock.release()
 
 
 def addGunshot(pt1, pt2):
@@ -288,7 +443,12 @@ def shoot():
     if intersecting:
         off = Vec2d(intersecting[0].rect.center) - Vec2d(hero.rect.center)
         if off.length < SCREEN_WIDTH:
-            intersecting[0].damage(10)
+            tgt = intersecting[0]
+            tgt.damage(10)
+            #if isinstance(tgt, Ally):
+                #multiplayer.s.send('6 %d %d;' % (tgt.n, 10))
+            #else:
+            #    multiplayer.s.send('5 %d %d;' % (tgt.n, 10)) 
             lines.add(((0,0,0), start, intersecting[0].rect.center))
         else:
             lines.add(((0,0,0), start, end))
@@ -303,6 +463,10 @@ def slash():
     collisions += pygame.sprite.spritecollide(hero, allies, False)
     for c in collisions:
         c.damage(30)
+        #if isinstance(c, Ally):
+            #multiplayer.s.send('6 %d %d;' % (c.n, 30))
+        #else:
+        #    multiplayer.s.send('5 %d %d;' % (c.n, 30) )
 
 def callHeli():
     print "geduda choppa"
@@ -375,10 +539,14 @@ while True:
             toUnload = loadedBlocks - shouldBeVisible
             print "Loading", toLoad
             print "Unloading", toUnload
+        #if multiplayer.hosting:
+        if True:
             for b in toLoad:
                 loadBlock(b)
+                #multiplayer.s.send('3 %d %d %d;' % (b[0], b[1], seeds[b]))
             for b in toUnload:
                 unloadBlock(b)
+                #multiplayer.s.send('4 %d %d;' % (b[0], b[1]))
             loadedBlocks = shouldBeVisible
             lastBlockLoad = pygame.time.get_ticks()
         hero.face(pygame.mouse.get_pos())
